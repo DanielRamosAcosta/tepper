cc3d3456784fe8f2c07ed7219b2713e6d3fce8bf
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-15 00:38:52 +0200

    increase timeout for node 18

--- src/utils/closePromised.ts
+++ src/utils/closePromised.ts
@@ -2,7 +2,9 @@ import { Server } from "http"
 
 export function closePromised(server: Server) {
   return new Promise<void>((resolve, reject) => {
+    console.time("server close")
     server.close((error) => {
+      console.timeEnd("server close")
       if (error) {
         reject(error)
       } else {
