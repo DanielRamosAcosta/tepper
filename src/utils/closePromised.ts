7a0194cd77183c879fc14d668ec2b84a6479bd7b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-15 18:01:34 +0200

    close all connections before closing the server

--- src/utils/closePromised.ts
+++ src/utils/closePromised.ts
@@ -2,9 +2,8 @@ import { Server } from "http"
 
 export function closePromised(server: Server) {
   return new Promise<void>((resolve, reject) => {
-    console.time("server close")
+    server.closeAllConnections()
     server.close((error) => {
-      console.timeEnd("server close")
       if (error) {
         reject(error)
       } else {
