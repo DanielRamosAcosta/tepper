4c44183683a54c300f52da49c896dd852b7a0570
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-16 19:07:31 +0200

    accept npm as package manager

--- test/e2e/nestjs/run.sh
+++ test/e2e/nestjs/run.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 cd test/e2e/nestjs
 
 rm -rf project-name
 
-npx @nestjs/cli new project-name
+yes npm | npx @nestjs/cli new project-name
 
 cd project-name
 
-npm i --save-dev ../../..
+npm install --save-dev ../../../..
 
 sed -ie '3d' test/app.e2e-spec.ts
 sed -i '' '3i\
 import { tepper } from "tepper"
+' test/app.e2e-spec.ts
+
+sed -ie '19d' test/app.e2e-spec.ts
+sed -i '' '19i\
+    return tepper(app.getHttpServer())
 ' test/app.e2e-spec.ts
 
 npm run test:e2e
