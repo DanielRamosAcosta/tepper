6e038395ec2201423fd4e0aa8f95411291073e05
Daniel Ramos <danielramosacosta1@gmail.com>
2023-12-26 16:45:34 +0000

    support only node 18 or higher

--- test/e2e/nestjs/run.sh
+++ test/e2e/nestjs/run.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-node_major_version=$(node --version | cut -d. -f1 | cut -dv -f2)
-
-if [ "$node_major_version" -lt 16 ]; then
-  echo "Node.js version is less than 16. Exiting with status 0."
-  exit 0
-else
-  echo "Node.js version is 16 or greater. Continuing..."
-fi
-
 cd test/e2e/nestjs
 
 rm -rf project-name clean.js
